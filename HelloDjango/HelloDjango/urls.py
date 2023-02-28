5d9034adb00eb07ce79305db831ce5bb5ba1a621
Konstantin Suvorov <91534415+ka-suvorov@users.noreply.github.com>
2023-02-28 18:52:01 +0300

    Refactor code

--- HelloDjango/HelloDjango/urls.py
+++ HelloDjango/HelloDjango/urls.py
@@ -3,16 +3,19 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include
 from django.urls import path, re_path
+from django.views.generic import TemplateView
 from django.views.static import serve
-from django.conf.urls import url
 
 
 urlpatterns = [
     path('', include('pages.urls')),
-    path('psy_wings/', include('psywings.urls')),
     path('ratings/', include('star_ratings.urls', namespace='ratings')),
+    path('ckeditor/', include('ckeditor_uploader.urls')),
     path('admin/', admin.site.urls),
-    ]
+
+    re_path('robots.txt', TemplateView.as_view(template_name="pages/robots.txt", content_type='text/plain')),
+]
+
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += [re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT,}),]
